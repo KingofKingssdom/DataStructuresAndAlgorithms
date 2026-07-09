bae2ba03408dd64cfdb1808eb96ae291b71d098d
Đức Huy <133219165+KingofKingssdom@users.noreply.github.com>
2026-07-09 07:49:39 +0700

    first commit

---
+++ Arrays/Test/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        /*Lý thuyết 
+         - Mảng là cấu trúc dữ liệu lưu trữ các phần tử có chung kiểu dữ liệu và được lưu trữ liền kề trong bộ nhớ
+         - Có kích thước cố định
+         - Truy cập phần tử mảng thông qua chỉ mục array[index], index có vị trí bắt đầu bằng 0
+         - Độ dài mảng là số lượng phần tử có trong mảng, dùng thuộc tính Length xác định. Nó đếm từ vị trí là 1 trở lên khác với index là từ 0
+         
+         */
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            int[] array1 = { };
+            int[] array2 = new int[] { };
+        }
+    }
+}
