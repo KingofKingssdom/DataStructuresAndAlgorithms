a1f20efd5ef5a8022d998c1ea0a4cf92412950ac
Đức Huy <133219165+KingofKingssdom@users.noreply.github.com>
2026-07-20 16:57:47 +0700

    update

---
+++ Algorithms/Searching/BinarySearch/Test/Test/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        /*Lý thuyết
+         Tìm kiếm nhị phân là tìm kiếm bằng cách chia nửa đoạn tìm kiếm ra thành 2 phần
+        - Độ phức tạp về thgian O(log N)
+        - Điều kiện để dùng được thuật toán tìm kiếm nhị phân là cấu trúc dữ liệu phải được sắp xếp
+        - Cách hoạt động
+            + Chia không gian tìm kiếm thành hai phần bằng cách tìm chỉ mục ở giữa "mid" .
+            + So sánh phần giữa của không gian tìm kiếm với khóa
+            + Nếu tìm thấy khóa ở giữa, quá trình sẽ bị chấm dứt.
+            + Nếu không tìm thấy khóa ở giữa, hãy chọn một nửa sẽ được sử dụng làm không gian tìm kiếm tiếp theo.
+              => Nếu khóa nhỏ hơn giữa, thì phía bên trái sẽ được sử dụng cho lần tìm kiếm tiếp theo.
+              => Nếu khóa lớn hơn giữa, thì phía bên phải sẽ được sử dụng cho lần tìm kiếm tiếp theo.
+            + Quá trình này được tiếp tục cho đến khi tìm thấy khóa hoặc toàn bộ không gian tìm kiếm đã được sử dụng hết.
+        - Có 2 cách triển khai
+            + Thuật toán tìm kiếm nhị phân lặp
+            + Thuật toán tìm kiếm nhị phân đệ quy
+         */
+
+        /*Triển khai tìm kiếm bằng nhị phân lặp*/
+        public static int binarySearch1(int[] array, int key)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (array[mid] == key)
+                {
+                    return mid;
+                }
+                else if (array[mid] < key)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return -1;
+        }
+        /*Triển khai bằng cách tìm kiếm đệ quy */
+        public static int binarySearch2(int[] array, int low, int high, int key)
+        {
+            if (high >= low)
+            {
+                int mid = low + (high - low) / 2;
+                if (array[mid] == key)
+                {
+                    return mid;
+                }
+                else if (array[mid] > key)
+                {
+                    return binarySearch2(array, low, mid - 1, key);
+                }
+                else
+                {
+                    return binarySearch2(array, mid + 1, high, key);
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 10, 40 };
+            int key = 10;
+            int result = binarySearch2(array, 0, array.Length - 1, key);
+            if (result == -1)
+                Console.WriteLine(
+                    "Element is not present in array");
+            else
+                Console.WriteLine("Element is present at "
+                                  + "index " + result);
+
+        }
+    }
+}
---
+++ Algorithms/Sorting/Test/Test/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        /*Lý thuyết 
+        - Sắp xếp là quá trình sắp xếp lại một mảng hoặc danh sách các phần tử đã cho theo một toán tử 
+        so sánh trên các phần tử đó. Toán tử so sánh được sử dụng để quyết định thứ tự mới của các phần tử trong cấu trúc dữ liệu tương ứng.
+        - Nguyên tắc cơ bản về sorting
+            + Sắp xếp tại chỗ (In-place sorting): Thuật toán sắp xếp tại chỗ sử dụng không gian bộ nhớ cố định để tạo ra kết quả (chỉ sửa đổi mảng đã cho). 
+                Ví dụ: Sắp xếp chọn, Sắp xếp nổi bọt, Sắp xếp chèn và Sắp xếp đống. 
+            + Sắp xếp nội bộ (Internal sorting): Là khi tất cả dữ liệu được lưu trữ trong  bộ nhớ chính  hoặc  bộ nhớ trong . 
+              Trong sắp xếp nội bộ, bài toán không thể nhận dữ liệu đầu vào vượt quá kích thước bộ nhớ được cấp phát.
+            + Sắp xếp ngoài (External sorting): Sắp xếp ngoài là khi toàn bộ dữ liệu cần được sắp xếp không nhất thiết phải được lưu trữ 
+              trong bộ nhớ cùng một lúc. Phương pháp sắp xếp này được gọi là sắp xếp ngoài. Sắp xếp ngoài được sử dụng cho lượng dữ liệu khổng lồ. 
+                Ví dụ, thuật toán sắp xếp trộn (Merge sort) có thể được sử dụng trong sắp xếp ngoài vì toàn bộ mảng không cần phải luôn có mặt trong bộ nhớ.
+            + Sắp xếp ổn định (Stable sorting): Khi hai phần tử giống nhau xuất hiện theo  cùng thứ tự  trong dữ liệu đã được sắp xếp như trong mảng ban đầu, đó được gọi là sắp xếp ổn định. 
+                Ví dụ: Sắp xếp trộn, Sắp xếp chèn, Sắp xếp nổi bọt
+            + Sắp xếp lai (Hybird sorting): Một thuật toán sắp xếp được gọi là lai nếu nó sử dụng nhiều hơn một thuật toán sắp xếp tiêu chuẩn để sắp xếp mảng. 
+              Ý tưởng là tận dụng ưu điểm của nhiều thuật toán sắp xếp. 
+                Ví dụ: IntroSort sử dụng sắp xếp chèn và sắp xếp nhanh.
+        - Các kĩ thuật phân loại: Trong cấu trúc dữ liệu, có nhiều thuật toán sắp xếp khác nhau được sử dụng. Có thể phân loại thuật toán sắp xếp thành hai loại chính 
+            + Dựa trên so sánh: Trong một thuật toán sắp xếp dựa trên việc so sánh, chúng ta sẽ so sánh các phần tử với nhau 
+               => cách vận hành: so sánh lần lượt số đầu tiên với số thứ hai, số thứ hai với số thứ ba
+            + Không dưa trên so sánh: Trong một thuật toán sắp xếp không dựa trên việc so sánh, chúng ta hoàn toàn không so sánh các phần tử với nhau
+               => cách vận hành: Dựa vào vị trí hoặc giá trị tuyệt đối của phần tử để xếp vào đúng chỗ. Giống như việc có bài kiểm tra từ 1 đến 5, có 5 cái hộp từ 1 đến 5 cứ cầm vào bài
+                  kiểm tra thấy điểm giá trị bao nhiêu ném vào hộp đúng giá trị đó. Cuối cùng cầm các bài từ hộp theo thứ tự là sắp xếp xong
+         */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
